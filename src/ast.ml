ad9a828c230e051c2342612990d7209de128decc
George Tamer <gtamer2@gmail.com>
2024-04-21 14:11:47 -0400

    Fix typo

--- src/ast.ml
+++ src/ast.ml
@@ -28,7 +28,7 @@ type expr =
   (* | Asn of string * Graph
   | Asn of string * primitive *)
   | Asn of string * expr
-  | Var of strings
+  | Var of string
   | Lit of int
   | FloatLit of float 
   | BoolLit of bool
