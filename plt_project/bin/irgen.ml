8f1c878251251c62e915ec260dfd04de3795cd25
George Tamer <gtamer2@gmail.com>
2024-05-09 15:57:58 +0100

    Temp commit, not working

--- plt_project/bin/irgen.ml
+++ plt_project/bin/irgen.ml
@@ -16,55 +16,59 @@ let translate sstmt_list sast_env =
   (* also need string *)
   (* and i8_ptr_t = L.pointer_type (L.i8_type context)  (* LLVM type for string *) *) 
   let i32_t = L.i32_type context
-  and f32_t = L.float_type context
-  and i1_t  = L.i1_type context in 
+  (* and f32_t = L.float_type context
+  and i1_t  = L.i1_type context in  *)
   
+  let builder = L.builder_at_end context (L.entry_block the_function) in
+
   (* Return the LLVM type for a GraphSQL type *)
   let ltype_of_typ = function
       A.Lit   -> i32_t
-    | A.FloatLit -> f32_t
-    | A.BoolLit  -> i1_t
+    (* | A.FloatLit -> f32_t
+    | A.BoolLit  -> i1_t *)
   in
 
   let 
 
   (* for finding values bounded to variable in string map *)
-  let lookup n env = try StringMap.find n (* *)
+  (* let lookup n env = try StringMap.find n (* *)
 
-  in
+  in *)
 
   let rec build_expr builder ((t, e) : sexpr) env = match e with
       SLit i -> L.const_int i32_t i
-    | SFloatLit f -> L.const_float f32_t f
+    (* | SFloatLit f -> L.const_float f32_t f
     | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
     | SVar s -> L.build_load (lookup s env) s builder
     | SAsn (s, e) -> build_expr builder e in 
-      ignore(L.build_store e' (lookup s) builder); e'
-    | SUniop (op, e1) -> 
+      ignore(L.build_store e' (lookup s) builder); e' *)
+    (* | SUniop (op, e1) -> 
       let e1' = build_expr builder e1 env in
         match op with 
-          A.Not -> L.build_not e1 "nottmp" builder
+          A.Not -> L.build_not e1 "nottmp" builder *)
     | SBinop (e1, op, e2) ->
       let e1' = build_expr builder e1
       and e2' = build_expr builder e2 in 
       match t, op with
           Int, Add   -> L.build_add e1' e2' "addtmp" builder
-        | Int, Sub   -> L.build_sub e1' e2' "subtmp" builder
-        | Int, Mul   -> L.build_mul e1' e2' "multmp" builder
-        | Int, Div   -> L.build_sdiv e1' e2' "divtmp" builder (* Signed division *)
-        | Int, Mod   -> L.build_srem e1' e2' "modtmp" builder (* Signed remainder *)
-        | Float, Add   -> L.build_fadd e1' e2' "addtmp" builder
+        (* | Int, Sub   -> L.build_sub e1' e2' "subtmp" builder
+        | Int, Mul   -> L.build_mul e1' e2' "multmp" builder *)
+         (* Signed division *)
+        (* | Int, Div   -> L.build_sdiv e1' e2' "divtmp" builder *)
+         (* Signed remainder *)
+        (* | Int, Mod   -> L.build_srem e1' e2' "modtmp" builder *)
+        (* | Float, Add   -> L.build_fadd e1' e2' "addtmp" builder
         | Float, Sub   -> L.build_fsub e1' e2' "subtmp" builder
         | Float, Mul   -> L.build_fmul e1' e2' "multmp" builder
-        | Float, Div   -> L.build_fdiv e1' e2' "divtmp" builder
-        | _, Eq    -> L.build_icmp L.Icmp.Eq e1' e2' "eqtmp" builder
+        | Float, Div   -> L.build_fdiv e1' e2' "divtmp" builder *)
+        (* | _, Eq    -> L.build_icmp L.Icmp.Eq e1' e2' "eqtmp" builder
         | _, Neq   -> L.build_icmp L.Icmp.Ne e1' e2' "neqtmp" builder
         | _, Gteq  -> L.build_icmp L.Icmp.Sge e1' e2' "geqtmp" builder
         | _, Lteq  -> L.build_icmp L.Icmp.Sle e1' e2' "leqtmp" builder
         | _, Gt    -> L.build_icmp L.Icmp.Sgt e1' e2' "gttmp" builder
         | _, Lt    -> L.build_icmp L.Icmp.Slt e1' e2' "lttmp" builder
         | _, And   -> L.build_and e1' e2' "andtmp" builder
-        | _, Or    -> L.build_or e1' e2' "ortmp" builder
+        | _, Or    -> L.build_or e1' e2' "ortmp" builder *)
       
 
       (* Scall - calling functions *)
@@ -92,17 +96,16 @@ let translate sstmt_list sast_env =
         builder'', env''
     in
 
-    let rec build_sstmt builder sstmt env = match sstmt with
-        SBlock sl -> build_sstmt_list builder sl env
-      | SExpr e -> ignore(build_expr builder e env); builder
-
-  in
-
+    let rec build_sstmt builder sstmt env = begin match sstmt with
+        (* SBlock sl -> build_sstmt_list builder sl env *)
+      | SExpr e -> ignore(build_expr builder e env);
+  end
 
 
 
-
-  let build_stmt builder = function
+  (* let build_stmt builder = function
     (* need ast to include statements *)
+    | A.Block sl -> build_sstmt_list builder sl
+    | A.Expr e -> ignore(build_expr builder e); *)
 
   
