0d423ed57cf41732558fc2cadfec84e5a92da1b8
George Tamer <gtamer2@gmail.com>
2024-05-12 15:08:03 +0100

    Irgen scaffold simple (#45)
    * initial irgen code
    
    * binop irgen
    
    * irgen adjustment in progress
    
    * looking into irgen framework
    
    * Temp commit, not working
    
    * Push
    
    * Semantic checker doesn't need empty env passed in... creates it itself. We did this for ease of use
    
    * M more changes (still not working)
    
    * scaffolding done
    
    * added main function and program entry point
    
    * assignment and calling variable done
    
    * fixed multiple entry points issuethe
    
    * added Binop constructors in build_expr
    
    * update Makefile
    
    * resolved conflict for semant_eval
    
    ---------
    
    Co-authored-by: AnvithPabba <anvith.pabba@gmail.com>
    Co-authored-by: lz-midway <lz2837@columbia.edu>
    Co-authored-by: Hellen wang <hellenwang@hellens-mbp.mynetworksettings.com>

--- plt_project/bin/irgen.ml
+++ plt_project/bin/irgen.ml
@@ -1,50 +1,84 @@
-
-
 module L = Llvm
 module A = Ast
 open Sast
 
-
 module StringMap = Map.Make(String)
 
+(* Helper function to check if a statement is an expression *)
+let is_expr = function
+  | SExpr _ -> true
+  | _ -> false
+
 let translate stmt_list =
-  (* print_string "test1/n"; *)
   let context = L.global_context () in
-  (* print_string "test2/n"; *)
-  (* create llvm module to generate code *)
   let the_module = L.create_module context "GraphSQL" in 
-  (* print_string "test3\n"; *)
-  let i32_t = L.i32_type context
-  in 
-  (* print_string "test4\n"; *)
+
+  let i32_t = L.i32_type context in 
   let builder = L.builder context in
-  (* print_string "test5\n"; *)
-  
-  let rec build_expr builder ((_, e) : sexpr) = match e with
-    | SLit i -> L.const_int i32_t i
-    | SBinop (e1, op, e2) ->
-      (* print_string "test6\n"; *)
-      let e11 = build_expr builder e1 in
-      (* print_string "test7\n"; *)
-      let e22 = build_expr builder e2 in 
-      (match op with
-          A.Add -> 
-            (* string_of_expr op; *)
-            L.build_add 
-          | _ -> raise (Invalid_argument "operation not supported")
-      ) e11 e22 "tmp" builder
-    | _ -> raise (Invalid_argument "expression type not supported")
-    (* end *)
-    in 
 
-  let rec build_sstmt builder sstmt = match sstmt with
-    | SExpr e -> ignore(build_expr builder e); builder
+    (* Return the LLVM type for a MicroC type *)
+  let ltype_of_typ = function
+    Int   -> i32_t
+  in
+
+  (* define the main function & program entry point *)
+  let main_type = L.function_type i32_t [||] in
+  let main_func = L.define_function "main" main_type the_module in
+
+  (* Create the entry block if it doesn't exist *)
+  let entry_block =
+    match L.entry_block main_func with
+    | block -> block
+    | _ -> L.append_block context "entry" main_func
+  in
+  L.position_at_end entry_block builder;
+
+  (* for now there is a single map, will need multiple maps, likely one for each function *)
+  let vars_map : L.llvalue StringMap.t = StringMap.empty in
+
+  let lookup n v_map = 
+    try Some (StringMap.find n v_map)
+    with Not_found -> None
+  in
+
+  let rec build_expr builder (t, e) vmap = match e with
+    | SLit i -> (L.const_int i32_t i), vmap
+    | SBinop (e1, op, e2) ->
+      let e1', vmap1 = build_expr builder e1 vmap in
+      let e2', vmap2 = build_expr builder e2 vmap1 in
+      begin match op with
+        | A.Add -> (L.build_add e1' e2' "add_tmp" builder), vmap2
+        | A.Sub -> (L.build_sub e1' e2' "sub_tmp" builder), vmap2
+        | A.Mul -> (L.build_mul e1' e2' "mult_tmp" builder), vmap2
+        | A.Div -> (L.build_sdiv e1' e2' "div_tmp" builder), vmap2
+        | A.Eq -> (L.build_icmp L.Icmp.Eq e1' e2' "eq" builder), vmap2
+        | A.Neq -> (L.build_icmp L.Icmp.Ne e1' e2' "eq" builder), vmap2
+        | A.Gt -> (L.build_icmp L.Icmp.Sgt e1' e2' "eq" builder), vmap2
+        | A.Lt -> (L.build_icmp L.Icmp.Slt e1' e2' "eq" builder), vmap2
+        | A.Gteq -> (L.build_icmp L.Icmp.Sge e1' e2' "eq" builder), vmap2
+        | A.Lteq -> (L.build_icmp L.Icmp.Sle e1' e2' "eq" builder), vmap2
+        | _ -> raise (Invalid_argument "operation not supported")
+      end
+    | SVar var -> (L.build_load (match (lookup var vmap) with 
+          Some v -> v
+        | None -> raise (Invalid_argument "variable not found")) var builder), vmap
+    | SAsn (s, e) -> let e', vmap' = build_expr builder e vmap in
+      let llval = (match (lookup s vmap') with
+          Some v -> v
+        | None -> L.build_alloca (ltype_of_typ t) s builder) in 
+      let vmap'' = StringMap.add s llval vmap' in
+      ignore(L.build_store e' llval builder); (e', vmap'')
+    | _ -> raise (Invalid_argument "expression type not supported")
+  in
+
+  let rec build_sstmt (builder, vmap) sstmt = match sstmt with
+    | SExpr e -> let _, vmap' = build_expr builder e vmap in builder, vmap'
+    | _ -> builder, vmap
   in 
 
-  let rec build_sstmt_list builder sstmt_list = match sstmt_list with
-    | [stmt] -> ignore (build_sstmt builder stmt); builder
-    | _ -> failwith "fail"
-  in
-  (* print_string "TEST TEST\n"; *)
-  build_sstmt_list builder stmt_list;
+  List.fold_left build_sstmt (builder, StringMap.empty) stmt_list;
+  (* List.iter (fun stmt -> ignore (build_sstmt builder stmt)) stmt_list; *)
+  (* add a return to the main function *)
+  let _ = L.build_ret (L.const_int i32_t 0) builder in
+
   the_module
